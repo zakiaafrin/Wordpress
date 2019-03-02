994a78fa9bae0f017b3ca979766e89b523167ea2
Zakia Afrin Jeme <zjeme@techlaunch.io>
2019-03-02 00:25:24 -0500

    Updated

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '@1234Nijhum');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
