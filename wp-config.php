6917bcff211d19d4957820b5952ba426271b7720
Zakia Afrin Jeme <zjeme@techlaunch.io>
2019-03-13 20:32:01 -0400

    Updated

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'wordpress');
-define('FS_METHOD','direct');
+define('FS_METHOD', 'direct');
 
 /** MySQL database username */
 define('DB_USER', 'wordpress');
